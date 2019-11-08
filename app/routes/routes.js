19ba66482048832a95d9ba2deee877bbd1c34d03
chbbs231 <atrobles231@gmail.com>
2019-11-07 19:55:55 -0800

    cleaned up

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,23 +2,15 @@ const path = require('path')
 
 module.exports = function(app){
 
-    app.get('/', function (req, res){ })
-
     app.get('/viewTables', function (req, res){
-        res.sendFile(path.json(__dirname,'..public/viewTables.html'))
+        res.sendFile(path.json(__dirname,'public' , 'viewTables.html'))
     })
 
     app.get('/reservation', function(req, res){
-        res.sendFile(path.json(___dirname, '..public/reservation.html'))
+        res.sendFile(path.json(___dirname, 'public', 'reservation.html'))
     })
 
-    app.get('*', function(req, res){
-        res.sendFile(path.json(___dirname, '...public/home.html'))
-    })
-
-    app.post('/reservation', function (req, res){
-        if( tableData.length > 5){
-
-        }
+    app.get('/', function(req, res){
+        res.sendFile(path.json(___dirname, 'public', 'home.html'))
     })
 }
