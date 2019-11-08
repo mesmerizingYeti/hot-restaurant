d2034af45bec618bc7016e279de5f9b1ae4d20ee
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-11-07 21:38:42 -0800

    Fixed sync errors in reservation.js

--- app/data/tableData.js
+++ app/data/tableData.js
@@ -4,6 +4,26 @@ const tableArr = [
     customerId: 'BW101',
     customerPhone: '(123) 456-7890',
     customerEmail: 'bruceWillis@gmail.com'
+  }, {
+    customerName: 'Bruce Willis',
+    customerId: 'BW101',
+    customerPhone: '(123) 456-7890',
+    customerEmail: 'bruceWillis@gmail.com'
+  }, {
+    customerName: 'Bruce Willis',
+    customerId: 'BW101',
+    customerPhone: '(123) 456-7890',
+    customerEmail: 'bruceWillis@gmail.com'
+  }, {
+    customerName: 'Bruce Willis',
+    customerId: 'BW101',
+    customerPhone: '(123) 456-7890',
+    customerEmail: 'bruceWillis@gmail.com'
+  }, {
+    customerName: 'Bruce Willis',
+    customerId: 'BW101',
+    customerPhone: '(123) 456-7890',
+    customerEmail: 'bruceWillis@gmail.com'
   }
 ]
 
--- app/public/assets/reservations.js
+++ app/public/assets/reservations.js
@@ -1,31 +1,31 @@
-let tables = []
+document.getElementById('submit').addEventListener('click', function (e) {
+  e.preventDefault()
 
-axios.get('http://localhost:3000/tables')
-  .then(r => tables = r.data)
-  .catch(e => console.log(e))
+  axios.get('http://localhost:3000/tables')
+    .then(({ data: tables }) => {
 
-  
-  document.getElementById('submit').addEventListener('click', function(e) {
-    e.preventDefault()
-    let name = document.getElementById('name')
-    let phone = document.getElementById('phone')
-    let email = document.getElementById('email')
-    let uniqueId = document.getElementById('uniqueId')
+      let name = document.getElementById('name')
+      let phone = document.getElementById('phone')
+      let email = document.getElementById('email')
+      let uniqueId = document.getElementById('uniqueId')
 
-    if(name.value === '' || phone.value === '' || email.value === '' || uniqueId ==='' ) {
-      alert('Enter all fields')
-    } else {
-    const customer = {
-      customerName: name.value,
-      customerId: uniqueId.value,
-      customerEmail: email.value,
-      customerPhone: phone.value
-    }
-      axios.post(`http://localhost:3000/${tables.length >= 5 ? 'waiting' : 'tables'}`, customer)
-      .catch(e => console.log(e))
-      document.getElementById('name').value = ''
-      document.getElementById('phone').value = ''
-      document.getElementById('email').value = ''
-      document.getElementById('uniqueId').value = ''
-
-    }
+      if (name.value === '' || phone.value === '' || email.value === '' || uniqueId === '') {
+        alert('Enter all fields')
+      } else {
+        const customer = {
+          customerName: name.value,
+          customerId: uniqueId.value,
+          customerEmail: email.value,
+          customerPhone: phone.value
+        }
+        console.log(tables.length)
+        axios.post(`http://localhost:3000/${tables.length >= 5 ? 'waiting' : 'tables'}`, customer)
+          .catch(e => console.log(e))
+        document.getElementById('name').value = ''
+        document.getElementById('phone').value = ''
+        document.getElementById('email').value = ''
+        document.getElementById('uniqueId').value = ''
+      }
+    })
+    .catch(e => console.log(e))
+})
