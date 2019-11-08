dd8b8c1ec229678220542e30089765e2f10ab6f7
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-11-07 20:59:50 -0800

    initial heroku push

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 
 app.use(express.static(path.join(__dirname, 'app', 'public')))
 app.use(express.json())
